83073c86641e592732a3961eb39b9fbb9d287edd
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-04-01 00:51:20 +0200

    Update .config/zsh/fzf-git.sh

--- dot_config/zsh/fzf-git.sh
+++ dot_config/zsh/fzf-git.sh
@@ -311,12 +311,12 @@ if [[ -n "${BASH_VERSION:-}" ]]; then
     local o c
     for o in "$@"; do
       c=${o:0:1}
-      bind -m emacs-standard '"\C-g\C-'$c'": " \C-u \C-a\C-k`_fzf_git_'$o'`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
-      bind -m vi-command     '"\C-g\C-'$c'": "\C-z\C-g\C-'$c'\C-z"'
-      bind -m vi-insert      '"\C-g\C-'$c'": "\C-z\C-g\C-'$c'\C-z"'
-      bind -m emacs-standard '"\C-g'$c'":    " \C-u \C-a\C-k`_fzf_git_'$o'`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
-      bind -m vi-command     '"\C-g'$c'":    "\C-z\C-g'$c'\C-z"'
-      bind -m vi-insert      '"\C-g'$c'":    "\C-z\C-g'$c'\C-z"'
+      bind -m emacs-standard '"\C-k\C-'$c'": " \C-u \C-a\C-k`_fzf_git_'$o'`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
+      bind -m vi-command     '"\C-k\C-'$c'": "\C-z\C-k\C-'$c'\C-z"'
+      bind -m vi-insert      '"\C-k\C-'$c'": "\C-z\C-k\C-'$c'\C-z"'
+      bind -m emacs-standard '"\C-k'$c'":    " \C-u \C-a\C-k`_fzf_git_'$o'`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
+      bind -m vi-command     '"\C-k'$c'":    "\C-z\C-k'$c'\C-z"'
+      bind -m vi-insert      '"\C-k'$c'":    "\C-z\C-k'$c'\C-z"'
     done
   }
 elif [[ -n "${ZSH_VERSION:-}" ]]; then
