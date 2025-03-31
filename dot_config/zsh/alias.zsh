b594f17808d03a3379d642747d7a7b19dd6a9292
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-03-31 22:21:20 +0200

    Add .config/key-bindings.md Update .config/zsh/alias.zsh

--- dot_config/zsh/alias.zsh
+++ dot_config/zsh/alias.zsh
@@ -1,88 +1,3 @@
-#🛠️ tools
-# git-open to open remote repository
-# fv -> search and open file in vim
-# fp -> copy pwd of the file
-# fls -> list files in directory
-# cx -> go to directory and list files
-# shift + ⌘ + x -> 🖼️ take screenshot
-# y -> file manager
-# gitstat -> summary of the git repository
-# cd - -> previous directory
-# z foo<SPACE><TAB>  # show interactive completions
-
-# 🚀 raycast 
-# ⌘ + space -> open raycast
-# alt + m -> open llm
-# hyper + ; -> open emojis 💜
-# 
-
-# 🌐 vivaldi
-# alt + c -> copy link 
-# ctrl + p -> toggle UI
-# ctrl + shift + <1-9>  #pick workspace
-
-#🔍 bindings
-# <c-r> # Search command history
-# <c-t> # Fuzzy find files
-# <c-e> # Fuzzy find directories
-
-#📺 Tmux bindings
-# <prefix> ?        # list all commands
-# <prefix> x        # close pane
-# <prefix> d       	# close current seesion
-# <prefix> |       	# split horizonatlyk
-# <prefix> -       	# split verticaly
-# <prefix> v        # toggle copy mode
-# when in copy mode
-# y                 # yank selection
-# gl                # move to end of line 
-# gh                # move to start of line
-# <prefix> <c-[/]> 	# move panes between spaces
-# <prefix> h/j/k/l 	# move between panes
-# <prefix> shift + f	# search actions
-# <prefix> <c-s>	# save session
-# <prefix> <c-r>	# restore session
-# <prefix> space	# which key
-# <prefix> s        # change session
-# <prefix> c        # create new winodow
-# esc + s           # in terminal to list tmux sessions
-
-#📋 move fzf-tabs
-# <- ',' '.' ->
-
-#🌲 git repo search
-# CTRL-G CTRL-F for Files
-# CTRL-G CTRL-B for Branches
-# CTRL-G CTRL-T for Tags
-# CTRL-G CTRL-R for Remotes
-# CTRL-G CTRL-H for commit Hashes
-# CTRL-G CTRL-S for Stashes
-# CTRL-G CTRL-L for reflogs
-# CTRL-G CTRL-W for Worktrees
-# CTRL-G CTRL-E for Each ref
-
-#🐁 install developer dependcies and tools
-# mise exec node@22 -- node -v  //install node
-# mise use --global node@22     //use global
-# mise use node@22              //create in directory mise.toml file and install dependency if not installed
-# mise install                  //installs everything specified in mise.toml
-# mise upgrade                  //upgrade tools and respec the version prefix node@22 will not bump to node 23
-# mise upgrade --bump node      //will upgrade tools to the latest available version
-# mise rm node@22               //remove node
-# mise ls                       //list installed tools
-# mise ls-remote node           //list remote tools versions
-
-#𐂷 pstree       shows process in tree like
-#⍙ delta -- to find diff between to files
-#🪵 [logfiles analyzer](https://docs.lnav.org)
-#🙈 [json manipulator](https://jqlang.org/)
-#👨 devtools for different part of manipulation
-#🕸️ [httpie](https://httpie.io/cli) network tool ⚠️  need to install
-#📗 [mactex](https://www.tug.org/mactex/) if you like to write in latex ⚠️ need to install
-#🚄 [bench](https://github.com/Gabriella439/bench) better time for commands ⚠ need to instal
-#📅 [meeting reminder](https://www.inyourface.app/)
-#📸 [image manipulation](https://imagemagick.org)
-#🌐 [wire shark](https://www.wireshark.org)
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 
 # Get week number
@@ -118,7 +33,7 @@ alias avv="branch -avv"
 alias vv="branch -vv"
 alias amen="commit --amend"
 
-alias ga="git add -p" #add only parts of the files to be staged
+alias ga="git add -p" # add only parts of the files to be staged
 alias gadd="git add"
 
 alias glog="git log --oneline --decorate --graph --all"
@@ -134,7 +49,7 @@ alias gr="git rebase"           #this is like cherry-pick but for manny commits
 alias gcp="git cherry-pick"
 
 alias gc="git commit -m"
-alias gca="git commit -a -m" #add files to be staged and commit
+alias gca="git commit -a -m" # add files to be staged and commit
 
 alias gpu="git pull origin"
 
