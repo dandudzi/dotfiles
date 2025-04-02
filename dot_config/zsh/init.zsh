4c06a6eeda50b2fd43f667422c8fe36ea7a2bec6
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-04-02 16:08:28 +0200

    Update .config/zsh/init.zsh

--- dot_config/zsh/init.zsh
+++ dot_config/zsh/init.zsh
@@ -71,7 +71,7 @@ setopt chaselinks
 ZSH_THEME="spaceship"
 
 #⚠️jira plugins require setup https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/jira
-plugins=(vi-mode chezmoi fzf mise alias-finder aliases aws common-aliases gradle docker mvn docker-compose gpg-agent jira kubectl python rust safe-paste spring sublime fzf-tab fzf-tab-source spaceship-vi-mode direnv)
+plugins=( chezmoi fzf mise alias-finder aliases aws common-aliases gradle docker mvn docker-compose gpg-agent jira kubectl python rust safe-paste spring sublime fzf-tab fzf-tab-source spaceship-vi-mode direnv)
 # enable alias finder for all coommands 
 zstyle ':omz:plugins:alias-finder' autoload yes # disabled by default
 zstyle ':omz:plugins:alias-finder' longer yes # disabled by default
