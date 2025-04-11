bc1400a2d3eed5bcad7611f3d024c89a8fe69b51
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-04-11 17:34:41 +0200

    Remove .config/zsh/brew.zsh

--- dot_config/zsh/brew.zsh
+++
@@ -1,9 +0,0 @@
-#🍻 Update brew once per day
-BREW_UPDATE_FILE="$HOME/.brew_last_update"
-# Run only if last update was over 24 hours ago
-if [ ! -f "$BREW_UPDATE_FILE" ] || [ $(date +%s) -gt $(($(cat "$BREW_UPDATE_FILE") + 86400)) ]; then
-    echo "Updating Homebrew... 🍺"
-    brew update && brew upgrade && brew cleanup
-    date +%s > "$BREW_UPDATE_FILE"  # Save current timestamp
-fi
- 
