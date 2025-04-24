6e6d73431c6663a7447fe82e5b965dda0d5601a3
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-04-24 21:03:38 +0200

    Update .config/zsh/update_packages.sh

--- dot_config/zsh/update_packages.sh
+++ dot_config/zsh/update_packages.sh
@@ -5,8 +5,9 @@ if [ ! -f "$UPDATE_FILE" ] || [ $(date +%s) -gt $(($(cat "$UPDATE_FILE") + 86400
   echo "Updating Homebrew... 🍺"
   brew update && brew upgrade && brew cleanup
 
-  echo "Updating homebrew file"
+  echo "Updating homebrew bundle file"
   brew bundle dump --file="~/.config/brewfile/Brewfile" --force
+  dota ~/.config/brewfile/Brewfile
 
   echo " Updating tpm plugins"
   ~/.config/tmux/plugins/tpm/bin/update_plugins all
