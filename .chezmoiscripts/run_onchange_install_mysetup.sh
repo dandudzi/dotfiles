a3dbb89c2da0ab2101990ee1b2f0f07bea2b54a6
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-06-24 13:20:33 +0200

    Update .chezmoiscripts/install_mysetup.sh

--- .chezmoiscripts/run_onchange_install_mysetup.sh
+++ .chezmoiscripts/run_onchange_install_mysetup.sh
@@ -95,6 +95,7 @@ echo "🔄 Updating Homebrew..."
 brew update
 
 echo "📦 Installing CLI tools..."
+brew bundle --file="~/.config/brewfile/Brewfile"
 
 echo "✅ Setup autocomplition for mise"
 mise completion zsh
