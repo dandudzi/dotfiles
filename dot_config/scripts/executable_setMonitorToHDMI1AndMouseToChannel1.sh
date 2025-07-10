cbbdb3e7f1fb06a2ae0920b800d144ae3949502c
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-07-10 22:28:29 +0200

    Add .config/scripts/setMonitorToDPAndMouseToChannel2.sh Add .config/scripts/setMonitorToHDMI1AndMouseToChannel1.sh Add .config/scripts/setMonitorToUSBCAndMouseToChannel3.sh

---
+++ dot_config/scripts/executable_setMonitorToHDMI1AndMouseToChannel1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title HDMI1
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Dudziak
+# @raycast.authorURL https://raycast.com/Dudziak
+
+./hidapitester/hidapitester --vidpid 046D:B020 --usagePage 0xFF43 --usage 0x0202 --open --length 20 --send-output 0x11,0x00,0x0C,0x1C,0x00
+./m1ddc/m1ddc set input 17
+osascript -e 'tell application "System Events" to keystroke "q" using {control down, command down}'
