5d0f6f3221aa809ffc884e363053c709a7a9349b
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-10-24 19:30:59 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/plugins/nvim-lspconfig.lua Add .config/nvim/lua/plugins/vim-tips.lua Add .config/nvim/neovim_tips/user_tips.md

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+map({ "n" }, "<leader>xm", function()
+    vim.cmd("messages")
+end, { desc = "Print Messages", expr = true, silent = true })
