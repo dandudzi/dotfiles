3ceb361e5082d4867a42c2be7095d7f2c68028de
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2025-11-12 17:11:25 +0100

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,23 @@
 local colorscheme = require("lazyvim.plugins.colorscheme")
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- catppuccin catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-      colorscheme = "catppuccin-macchiato",
-      -- colorscheme = "catppuccin",
-      -- colorscheme = "eldritch",
-      -- colorscheme = "catppuccin-frappe",
-      -- colorscheme = "Duskfox",
-      -- colorscheme = "Nightfox",
-      -- colorscheme = "Carbonfox",
-      -- colorscheme = "gruvbox",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            -- catppuccin catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+            colorscheme = "catppuccin-macchiato",
+            -- colorscheme = "catppuccin",
+            -- colorscheme = "eldritch",
+            -- colorscheme = "catppuccin-frappe",
+            -- colorscheme = "Duskfox",
+            -- colorscheme = "Nightfox",
+            -- colorscheme = "Carbonfox",
+            -- colorscheme = "gruvbox",
+        },
     },
-  },
+    {
+        "catppuccin",
+        opts = {
+            transparent_background = true,
+        },
+    },
 }
