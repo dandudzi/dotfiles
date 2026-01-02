957f3b20f45fe3041476344318ea288a32a72585
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2026-01-02 14:04:51 +0100

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,31 @@ local map = LazyVim.safe_keymap_set
 map({ "n" }, "<leader>xm", function()
     vim.cmd("messages")
 end, { desc = "Print Messages", expr = true, silent = true })
+
+vim.api.nvim_create_user_command("NextNumberedFile", function()
+    local buf = vim.api.nvim_get_current_buf()
+    local name = vim.fn.expand("%:t")
+    local dir = vim.fn.expand("%:p:h")
+
+    local num, rest = name:match("^(%d%d)_(.+)$")
+    if not num then
+        vim.notify("Filename does not match NN_xxxx format", vim.log.levels.ERROR)
+        return
+    end
+
+    local next_num = string.format("%02d", tonumber(num) + 1)
+    local pattern = dir .. "/" .. next_num .. "_*"
+
+    local matches = vim.fn.glob(pattern, false, true)
+    if #matches == 0 then
+        vim.notify("No file starting with " .. next_num .. "_ found", vim.log.levels.WARN)
+        return
+    end
+    -- open first match (sorted by glob)
+    vim.cmd("edit " .. vim.fn.fnameescape(matches[1]))
+
+    -- close previous buffer without saving
+    vim.api.nvim_buf_delete(buf, { force = true }) -- open next file
+end, {})
+
+vim.keymap.set("n", "<leader>1", ":NextNumberedFile<CR>", { silent = true })
