9298387cdac102741c72370a16289b39ab9b5f55
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2026-02-18 19:16:44 +0100

    Update .config/sketchybar/settings.lua

--- dot_config/sketchybar/settings.lua
+++ dot_config/sketchybar/settings.lua
@@ -2,8 +2,8 @@ return {
 	paddings = 8,
 	group_paddings = 5,
 	font = {
-		text = "CommitMono", -- Used for text
-		numbers = "CommitMono", -- Used for numbers
+		text = "JetBrains Mono", -- Used for text
+		numbers = "JetBrains Mono", -- Used for numbers
 		app_icons = "sketchybar-app-font",
 		icons = "Symbols Nerd Font",
 		style_map = {
