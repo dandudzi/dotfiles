291aa78937fc8849d8b4bc4ba02d5caf56de81f2
Daniel Dudziak <20063579+dandudzi@users.noreply.github.com>
2026-02-21 20:23:51 +0100

    Update .config/nvim/lua/plugins/hardtime.lua

--- dot_config/nvim/lua/plugins/hardtime.lua
+++ dot_config/nvim/lua/plugins/hardtime.lua
@@ -6,4 +6,18 @@ return {
     },
     event = "VeryLazy",
     opts = {},
+    hints = {
+        ["[dcyvV][ia][%(%)]"] = {
+            message = function(keys)
+                return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
+            end,
+            length = 3,
+        },
+        ["[dcyvV][ia][%{%}]"] = {
+            message = function(keys)
+                return "Use " .. keys:sub(1, 2) .. "B instead of " .. keys
+            end,
+            length = 3,
+        },
+    },
 }
